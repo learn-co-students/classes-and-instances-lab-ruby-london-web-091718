be2e01ec7f06904900e7e6ecb6086f53eb4b8c30
Irfan Suleman <ivsuleman@gmail.com>
2018-09-21 11:32:24 +0100

    fixed

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+# 1. Define Dog in lib/dog.rb
+# Open lib/dog.rb and add a class definition for a Dog class.
+
+# 2. Make 3 dogs in lib/dog.rb
+# Under your Dog class definition, create three dogs in local variables, fido, snoopy, and lassie.
+
+# 3. Define a Person in lib/person.rb
+# Open lib/person.rb and add a class definition for a Person class.
+
+# 4. Make 2 people in lib/person.rb
+# Under your Person class definition, create two people in local variables, adele_goldberg and alan_kay
+
+# When you're done, submit the lab with learn submit.
